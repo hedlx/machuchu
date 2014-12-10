9a75d81c284563f6abc34b1dd094e5d70e0ab0b6
Yaroslav Kurlaev <yaroslav.kurlaev@gmail.com>
2014-12-10 22:30:26 +0600

    spiral'ka

--- moire.f
+++ moire.f
@@ -3,8 +3,8 @@
 varying vec4 p;
 
 uniform float time;
-//uniform float s = 1.;
-uniform float c = 1.;
+//niform float s = 1.;
+//niform float c = 1.;
 
 #define mod(x, k) (1 - abs((x) - int((x) / (k)) * (k) - 1))
 #define div(x, k) (int((x) / (k)))
@@ -32,7 +32,7 @@ void main()
     pp.x = -(pp.x + 1.) / 2.;
     float t = (time / 10000.);
     pp.y = (pp.y - 1.) / 2;
-    float f = /*pow(t, 2) * abs(pp.x) / abs(pp.y); */(pow(t, 2) * (pp.x * pp.x + pp.y * pp.y));
+    float f = /*pow(t, 2) * abs(pp.x) / abs(pp.y); */(pow(t, 2) * (/*sqrt*/(pp.x * pp.x + pp.y * pp.y) - atan(pp.y / pp.x)));
 //    f = (s == 0.) ? f : sin(2 * 3.14159265358979323846 * f);
 //    f -= int(f);
     gl_FragColor = hsv2rgb(mod(f / t, 1.), mod(f / (t * t), 1.), 1.);//mod(f, 1.), 1./*mod((f - mod(f, 1.)) / 256., 1.)*/, 1.);
