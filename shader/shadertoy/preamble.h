f79c59ad3ebc2473fea97ea1848198d55923e1af
Albert Safin <xzfcpw@gmail.com>
2020-03-23 21:44:32 +0000

    Add some shadertoy shaders

---
+++ shader/shadertoy/preamble.h
@@ -0,0 +1,11 @@
+uniform float time;
+varying vec4 p;
+#define iTime (time/1000.)
+#define iMouse (vec4(0, 0, 0, 0))
+const vec2 iResolution = vec2(640, 360);
+uniform sampler2D iChannel0;
+uniform sampler2D iChannel1;
+uniform sampler2D iChannel2;
+uniform sampler2D iChannel3;
+void mainImage(out vec4 c, vec2 u);
+void main() { mainImage(gl_FragColor, p.xy*360./2. + iResolution/2.); }
