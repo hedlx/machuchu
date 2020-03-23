7c37fec482cf8563fe1e4454a24fd48f0370dee8
Albert Safin <xzfcpw@gmail.com>
2020-03-23 21:57:26 +0000

    Fix undefined behavior in imod

--- shader/lib.h
+++ shader/lib.h
@@ -10,7 +10,7 @@ const float TAU = radians(360);
 #define map3(f, v) vec3(f((v).x), f((v).y), f((v).z))
 
 int   idiv (int a, int b) { return (a + (a>0?b-1:0))/b; }
-int   imod (int a, int b) { return a>=0 ? a%b : b-1+(a+1)%b; }
+int   imod (int a, int b) { return a>=0 ? a%b : b-1-(b-1-a)%b; }
 float add2 (vec2 a) { return a.x + a.y; }
 float sub2 (vec2 a) { return a.x - a.y; }
 bool  bxor2(bvec2 a) { return a.x ^^ a.y; }
